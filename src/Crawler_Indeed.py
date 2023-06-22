ed93d30bc19f0797b3cbf51d3e6973143cd706d8
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2023-06-22 14:24:26 +0200

    Konstruktiv Verbesserungen

--- src/Crawler_Indeed.py
+++ src/Crawler_Indeed.py
@@ -1,3 +1,4 @@
+from sentence_transformers import SentenceTransformer, util
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -17,9 +18,11 @@ class Crawler_Indeed():
     # @Parameter: url; Gibt die URL, der zu extrahierenden Webseite an
     # @Paramter: name_csv; Gibt an, unter welchem Namen die CSV-Datei gespeichert werden soll 
 
-    def __init__(self, url,  name_csv):
+    def __init__(self, url,  name_csv, example_sentences, label ):
         self.url = url
         self.name_csv = name_csv
+        self.example_sentences = example_sentences
+        self.label = label
 
     # In dieser Function werden die Daten aus der Indeed Webseite ausgelesen 
     # Die extrahierten Saetze über die jeweiligen Unternehmen die ausgelesen wurden, werden in einer CSV-Datei gespeichert
@@ -37,7 +40,7 @@ class Crawler_Indeed():
             for job in driver.find_elements(By.CLASS_NAME, "job-snippet"):
                 soup = BeautifulSoup(job.get_attribute('innerHTML'),'html.parser')   
                 try:
-                     description = soup.find("li").text.replace("\n","").strip()
+                     description = soup.find("li").text.replace("\n","").strip() # type: ignore
                 except:
                     description = 'None'  
                 data_list.append(description)
@@ -56,16 +59,27 @@ class Crawler_Indeed():
     # @Return: eine Liste der Eingabesequenzen und eine Liste der zugehoerigen Zielvariablen 
 
     def get_labels_for_each_sentence(self): 
-        data_list = self.extract_Data()
+        company_info_sentences = self.extract_Data()
         label_list=[]
-        for sentence in range(0,len(data_list)):
-            if "e-commerce" in self.url and ("Webentwicklung" in data_list[sentence] or "E-Commerce" in data_list[sentence]):
-                label_list.append("Softwareentwicklung im E-Commerce")
-            elif "bank" in self.url and ("Banken" in data_list[sentence] or "bank" in data_list[sentence]):
-                label_list.append("Softwareentwicklung im Bankensektor")
-            elif "Cloud" in self.url and ("Cloud" in data_list[sentence] or "cloud" in data_list[sentence]):
-                label_list.append("Softwareentwicklung für Cloud-Lösungen")
-            else:
-                label_list.append("Sonstiges")
-            
-        return data_list, label_list
+        
+        model = SentenceTransformer('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
+        embeddings1 = model.encode(self.example_sentences, convert_to_tensor=True)
+        embeddings2 = model.encode(company_info_sentences, convert_to_tensor=True)
+        cosine_scores = util.cos_sim(embeddings1, embeddings2) # type: ignore
+
+        for j in range(len(cosine_scores)):
+            for i in range(len(company_info_sentences)):
+                if cosine_scores[j][i] > 0.5: 
+                    label_list.append(self.label)
+                else: 
+                    label_list.append('Sonstiges')
+
+        count = 0 
+        for label in label_list: 
+            if label == 'Sonstiges': 
+                count=+1
+        
+        if count == len(label_list): 
+            print('Nach der Firmenbeschreibung, ist die Firma: nicht in der Branche taetig')
+
+        return company_info_sentences, label_list
