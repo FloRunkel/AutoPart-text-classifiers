26ac205e3f656bc38d282f315acceb272b1d039b
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2023-08-03 12:45:05 +0200

    Dependencys

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools import find_namespace_packages
+
+setup(
+    include_package_data=True,
+    name='AutoPart-text-classifiers',
+    version='1.0.0',
+    author='Florian Runkel',
+    author_email='f.runkel@yahoo.com',
+    description=' Classifier zur Klassifikation von Unternehmen ',
+    packages=find_namespace_packages(),
+
+    # Here is the URL where you can find the code
+    url='https://github.com/FloRunkel/AutoPart-text-classifiers/tree/main/src',
+
+    install_requires=[
+        'scikit-learn',
+        'simpletransformers',
+        'selenium',
+        'webdriver_manager',
+        'bs4',
+        'pandas',
+        'urllib3',
+        'requests',
+        'wikipedia-api',
+        'sentence_transformers',
+        'matplotlib',
+        'sklearn',
+        'spacy',
+        'flair',
+        'transformers',
+    ],
+
+    python_requires='>=3.7',
+
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Textklassifikation',
+    ]
+)
